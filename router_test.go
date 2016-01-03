837527d5ed49ab99c44c69574945d7a6765c2a7b
Дмитрий Седых <dmitrys@xyzrd.com>
2016-01-03 06:09:51 +0300

    Базовый объект для определения роутинга

---
+++ router_test.go
@@ -0,0 +1,49 @@
+package rest
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/kr/pretty"
+)
+
+var urls = []string{
+	"/user",
+	"/user/test",
+	"/user/:id",
+	"/user/:id/param",
+	"///user/:id/param1/param2/:id/param3//",
+}
+
+func TestSplit(t *testing.T) {
+	for _, url := range urls {
+		splitted := split(url)
+		fmt.Printf("%q\n", splitted)
+		if strings.Join(splitted, "/") != strings.Trim(url, "/") {
+			t.Error(url, splitted)
+		}
+	}
+}
+
+func TestRouter(t *testing.T) {
+	var r router
+	for _, url := range urls {
+		if err := r.add(url, url); err != nil {
+			t.Error(err)
+		}
+	}
+	for _, url := range urls {
+		handler, params := r.lookup(url)
+		if handler == nil {
+			t.Error("Nil handler:", url)
+		}
+		pretty.Println(handler, params)
+	}
+	url := "/user/:id/param1/"
+	handler, params := r.lookup(url)
+	if handler != nil {
+		t.Error("Bad handler:", url)
+	}
+	pretty.Println(handler, params)
+}

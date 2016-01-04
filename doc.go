7c890e031766052c6798f67a9952029772f94f06
Дмитрий Седых <dmitrys@xyzrd.com>
2016-01-04 07:22:26 +0300

    docs

--- doc.go
+++ doc.go
@@ -4,26 +4,26 @@
 // часто используемые вещи, поддерживает параметры в пути, совместима со стандартной библиотекой
 // http и позволяет описывать обработчики в таком виде, как удобно мне.
 //
-//	mux.Handles(Handlers{
-//		"/user/:id": {
-//			"GET": rest.HandlerFunc(func(c *rest.Context) {
-//				c.Body(rest.JSON{"user": c.Get("id")})
-//			}),
-//			"POST": rest.HandlerFunc(func(c *rest.Context) {
-//				var data = make(rest.JSON)
-//				if err := c.Parse(&data); err != nil {
-//					c.Code(500).Body(err)
-//					return
-//				}
-//				c.Body(rest.JSON{"user": c.Get("id"), "data": data})
-//			}),
-//		},
-//		"/message/:text": {
-//			"GET": rest.HandlerFunc(func(c *rest.Context) {
-//				c.Body(rest.JSON{"message": c.Get("text")})
-//			}),
-//		},
-//	})
+//  mux.Handles(Handlers{
+//      "/user/:id": {
+//          "GET": rest.HandlerFunc(func(c *rest.Context) {
+//              c.Body(rest.JSON{"user": c.Get("id")})
+//          }),
+//          "POST": rest.HandlerFunc(func(c *rest.Context) {
+//              var data = make(rest.JSON)
+//              if err := c.Parse(&data); err != nil {
+//                  c.Code(500).Body(err)
+//                  return
+//              }
+//              c.Body(rest.JSON{"user": c.Get("id"), "data": data})
+//          }),
+//      },
+//      "/message/:text": {
+//          "GET": rest.HandlerFunc(func(c *rest.Context) {
+//              c.Body(rest.JSON{"message": c.Get("text")})
+//          }),
+//      },
+//  })
 //
 // Вообще, библиотека написана исключительно для внутреннего использования и нет никаких
 // гарантий, что она не будет время от времени серьезно изменяться. Поэтому, если вы хотите

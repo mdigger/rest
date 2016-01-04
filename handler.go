59abee828e8d5d165aeccefc08ace17b47e0ffdc
Дмитрий Седых <dmitrys@xyzrd.com>
2016-01-04 04:34:25 +0300

    handler

--- handler.go
+++ handler.go
@@ -19,12 +19,25 @@ func (f HandlerFunc) ServeHTTPC(c *Context) { f(c) }
 // Method описывает список Handle, ассоциированные с HTTP-методами.
 type Method map[string]Handler
 
+type Handlers map[string]Method
+
 // ServeMux описывает список обработчиков, ассоциированных с путями запроса и методами.
 type ServeMux struct {
 	// Глобальный обработчик, вызываемый перед всеми заданными обработчиками,
 	// если определен.
 	CustomHandler func(Handler) Handler
 	router        // обработчики запросов по путям, без учета метода запроса
+}
+
+// Handles добавляет определение обработчиков сразу для всех методов для указанного пути.
+func (m *ServeMux) Handles(handlers Handlers) {
+	for path, methods := range handlers {
+		if len(methods) > 0 {
+			if err := m.router.add(path, methods); err != nil {
+				panic(err)
+			}
+		}
+	}
 }
 
 // Handle добавляет новый обработчик для указанного пути и метода запроса.
@@ -42,55 +55,18 @@ func (m *ServeMux) Handle(method, path string, handler Handler) {
 		}
 	}
 	// обработчик для данного пути не определен
-	m.Handles(path, Method{method: handler})
-}
-
-// HandleFunc позволяет использовать в качестве обработчика функцию, принимающую контекст в
-// качестве аргумента.
-func (m *ServeMux) HandleFunc(method, path string, handler func(*Context)) {
-	m.Handle(method, path, HandlerFunc(handler))
-}
-
-// Handles добавляет определение обработчиков сразу для всех методов для указанного пути.
-func (m *ServeMux) Handles(path string, handlers Method) {
-	if len(handlers) == 0 {
-		return
-	}
-	if err := m.router.add(path, handlers); err != nil {
-		panic(err)
-	}
+	m.Handles(Handlers{path: Method{method: handler}})
 }
 
 // Handler позволяет привязать к нашему описанию стандартный обработчик http.Handler.
 func (m *ServeMux) Handler(method, path string, handler http.Handler) {
-	m.HandleFunc(method, path, func(c *Context) {
+	m.Handle(method, path, HandlerFunc(func(c *Context) {
 		handler.ServeHTTP(c.Response, c.Request)
-	})
-}
-
-// HandlerFunc позволяет привязать к нашему описанию стандартный обработчик http.Handler.
-func (m *ServeMux) HandlerFunc(method, path string, handler http.HandlerFunc) {
-	m.HandleFunc(method, path, func(c *Context) {
-		handler(c.Response, c.Request)
-	})
-}
-
-// Lookup возвращает обработчик для указанного пути и метода, а так же заполненный список
-// параметров пути. Если обработчик для данных параметров не определен, то возвращается nil.
-func (m *ServeMux) Lookup(method, path string) (h Handler, params Params) {
-	route, params := m.router.lookup(path)
-	if route == nil {
-		return
-	}
-	// в роутере хранятся обработчики с привязкой к методам
-	if methods, ok := route.(Method); ok {
-		h = methods[strings.ToUpper(method)]
-	}
-	return
+	}))
 }
 
 // ServeHTTPC поддерживает интерфейс Handler и отвечает за основную обработку запроса.
-func (m *ServeMux) ServeHTTPC(context *Context) {
+func (m ServeMux) ServeHTTPC(context *Context) {
 	// получаем обработчик для указанного пути
 	route, params := m.router.lookup(context.Request.URL.Path)
 	context.Params = append(context.Params, params...)
@@ -122,6 +98,6 @@ func (m *ServeMux) ServeHTTPC(context *Context) {
 }
 
 // ServeHTTP обеспечивает поддержку интерфейса http.Handler и обрабатывает основной запрос.
-func (m *ServeMux) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+func (m ServeMux) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	m.ServeHTTPC(newContext(w, req, nil)) // формируем контекст для ответа
 }

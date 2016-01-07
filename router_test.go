241017cd7bb1f6e224633826a712ebb8599f9357
Дмитрий Седых <dmitrys@xyzrd.com>
2016-01-07 09:37:59 +0300

    router test

--- router_test.go
+++ router_test.go
@@ -4,16 +4,14 @@ import (
 	"fmt"
 	"strings"
 	"testing"
-
-	"github.com/kr/pretty"
 )
 
 var urls = []string{
-	"/user",
-	"/user/test",
-	"/user/:id",
-	"/user/:id/param",
 	"///user/:id/param1/param2/:id/param3//",
+	"/user/:id/param",
+	"/user/:id",
+	"/user/test",
+	"/user",
 }
 
 func TestSplit(t *testing.T) {
@@ -38,12 +36,53 @@ func TestRouter(t *testing.T) {
 		if handler == nil {
 			t.Error("Nil handler:", url)
 		}
-		pretty.Println(handler, params)
+		fmt.Println(handler, params)
 	}
 	url := "/user/:id/param1/"
 	handler, params := r.lookup(url)
 	if handler != nil {
 		t.Error("Bad handler:", url)
 	}
-	pretty.Println(handler, params)
+	fmt.Println(handler, params)
+	handler, params = r.lookup("/user/test/mama/1/2/3/4/5/6/7/8/9/0/")
+	if handler != nil {
+		t.Error("Bad handler:", url)
+	}
+}
+
+func TestLongRouter(t *testing.T) {
+	var r router
+	url := `1/2/3/4/5/6/7/8/9/0/1/2/3/4/5/6/7/8/9/0/1/2/3/4/5/6/7/8/9/0/1/2/3/4/5/6/7/8/9/0/1/2/3/4/5/6/7/8/9/0/` +
+		`1/2/3/4/5/6/7/8/9/0/1/2/3/4/5/6/7/8/9/0/1/2/3/4/5/6/7/8/9/0/1/2/3/4/5/6/7/8/9/0/1/2/3/4/5/6/7/8/9/0/` +
+		`1/2/3/4/5/6/7/8/9/0/1/2/3/4/5/6/7/8/9/0/1/2/3/4/5/6/7/8/9/0/1/2/3/4/5/6/7/8/9/0/1/2/3/4/5/6/7/8/9/0/` +
+		`1/2/3/4/5/6/7/8/9/0/1/2/3/4/5/6/7/8/9/0/1/2/3/4/5/6/7/8/9/0/1/2/3/4/5/6/7/8/9/0/1/2/3/4/5/6/7/8/9/0/` +
+		`1/2/3/4/5/6/7/8/9/0/1/2/3/4/5/6/7/8/9/0/1/2/3/4/5/6/7/8/9/0/1/2/3/4/5/6/7/8/9/0/1/2/3/4/5/6/7/8/9/0/` +
+		`1/2/3/4/5/6/7/8/9/0/1/2/3/4/5/6/7/8/9/0/1/2/3/4/5/6/7/8/9/0/1/2/3/4/5/6/7/8/9/0/1/2/3/4/5/6/7/8/9/0/`
+	if err := r.add(url, "long"); err == nil {
+		t.Error("must be error")
+	}
+}
+
+func TestOnlyStaticRouter(t *testing.T) {
+	var r router
+	var urls = []string{
+		"test/url",
+		"test2/url",
+		"test2/add",
+		"test/add",
+		"test",
+	}
+	for _, url := range urls {
+		if err := r.add(url, "long"); err != nil {
+			t.Error(err)
+		}
+	}
+	handler, _ := r.lookup("/test")
+	if handler == nil {
+		t.Error("Bad handler:", "/test")
+	}
+	handler, _ = r.lookup("/test2")
+	if handler != nil {
+		t.Error("Bad handler:", "/test2")
+	}
 }

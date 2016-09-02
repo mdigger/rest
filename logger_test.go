3d9f0e2f550f76d08187cb70832cacd7fa500f47
Дмитрий Седых <dmitrys@xyzrd.com>
2016-09-02 06:53:58 +0300

    tests

--- logger_test.go
+++ logger_test.go
@@ -2,12 +2,19 @@ package rest
 
 import (
 	"errors"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
+	"net/http/httputil"
 	"os"
 	"strings"
 	"testing"
 )
+
+func init() {
+	Compress = false
+	Debug = true
+}
 
 func TestLogger(t *testing.T) {
 	for i, method := range []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS", "CONNECT"} {
@@ -28,7 +35,6 @@ func TestLogger(t *testing.T) {
 }
 
 func TestLogger2(t *testing.T) {
-	Debug = true
 	SetLogger(os.Stderr)
 	var mux ServeMux
 	mux.Handle("GET", "/test", func(c *Context) error { return c.Send("test1") })
@@ -53,11 +59,11 @@ func TestLogger2(t *testing.T) {
 		if err != nil {
 			t.Error(err)
 		}
-		// dump, err := httputil.DumpResponse(resp, true)
-		// if err != nil {
-		// 	t.Fatal(err)
-		// }
-		// fmt.Printf("%s\n%s\n", dump, strings.Repeat("-", 40))
+		dump, err := httputil.DumpResponse(resp, true)
+		if err != nil {
+			t.Fatal(err)
+		}
+		fmt.Printf("%s\n%s\n", dump, strings.Repeat("-", 40))
 		resp.Body.Close()
 	}
 }
@@ -66,7 +72,6 @@ func test1(c *Context) error { return errors.New("test error") }
 func test2(c *Context) error { panic("panic error"); return nil }
 
 func TestLoggerError(t *testing.T) {
-	Debug = true
 	SetLogger(os.Stderr)
 	var mux ServeMux
 	mux.Handle("GET", "/test", test1)
@@ -82,6 +87,41 @@ func TestLoggerError(t *testing.T) {
 		if err != nil {
 			t.Error(err)
 		}
+		resp.Body.Close()
+	}
+}
+
+func TestLogger3(t *testing.T) {
+	SetLogger(nil)
+	defer SetLogger(os.Stderr)
+	var mux ServeMux
+	mux.Handle("GET", "/test", func(c *Context) error { return c.Send("test1") })
+	mux.Handle("POST", "/test", func(c *Context) error { return c.Send(errors.New("test2")) })
+	mux.Handle("PUT", "/test", func(c *Context) error { return c.Send([]byte("test3")) })
+	mux.Handle("PATCH", "/test", func(c *Context) error { return c.Send(strings.NewReader("test4")) })
+	mux.Handle("TEST", "/test", func(c *Context) error {
+		return c.Send(struct {
+			Test complex64
+		}{
+			Test: complex(3, 15),
+		})
+	})
+	ts := httptest.NewServer(mux)
+	defer ts.Close()
+	for _, method := range []string{"GET", "POST", "PUT", "PATCH", "TEST"} {
+		req, err := http.NewRequest(method, ts.URL+"/test", nil)
+		if err != nil {
+			t.Fatal(err)
+		}
+		resp, err := http.DefaultClient.Do(req)
+		if err != nil {
+			t.Error(err)
+		}
+		dump, err := httputil.DumpResponse(resp, true)
+		if err != nil {
+			t.Fatal(err)
+		}
+		fmt.Printf("%s\n%s\n", dump, strings.Repeat("-", 40))
 		resp.Body.Close()
 	}
 }

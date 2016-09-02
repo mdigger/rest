beafce4eb3c80fbbe16d7e135181d5f643b54103
Дмитрий Седых <dmitrys@xyzrd.com>
2016-09-02 05:57:09 +0300

    expvar support

--- expvar/expvar.go
+++ expvar/expvar.go
@@ -1,16 +1,42 @@
-// Package debugvar добавляет поддержку expvar в библиотеку rest.
-package debugvar
+// Package expvar добавляет поддержку стандартной библиотеки expvar в rest.
+//
+// При подключении библиотеки к проекту автоматически стандартные обработчики
+// определенные в expvar, такие как "cmdline" и "memstats". Кроме этого, данная
+// библиотека так же регистрирует "goroutines", содержащее число запущенных
+// горутин. И "uptime" с количеством наносекунд, прошедших с момента старта
+// сервиса.
+package expvar
 
 import (
 	"expvar"
 	"fmt"
+	"runtime"
+	"time"
 
 	"github.com/mdigger/rest"
 )
 
-// Path описывает путь обработчика для ExpVar. В отличии от стандартной
-// библиотеки, вы можете изменить здесь этот путь.
-var Path = "/debug/vars"
+var (
+	// Path описывает путь обработчика для ExpVar. В отличии от стандартной
+	// библиотеки, вы можете изменить здесь этот путь.
+	Path      = "/debug/vars"
+	startTime = time.Now().UTC() // время запуска сервиса
+)
+
+// goroutines is an expvar.Func compliant wrapper for runtime.NumGoroutine function.
+func goroutines() interface{} {
+	return runtime.NumGoroutine()
+}
+
+// uptime is an expvar.Func compliant wrapper for uptime info.
+func uptime() interface{} {
+	return int64(time.Since(startTime))
+}
+
+func init() {
+	expvar.Publish("goroutines", expvar.Func(goroutines))
+	expvar.Publish("uptime", expvar.Func(uptime))
+}
 
 // Register регистрирует обработчик ExpVar среди обработчиков данного
 // сервера. Используется для совместимости со стандартной библиотекой expvar.

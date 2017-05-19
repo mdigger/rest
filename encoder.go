f7dd5dc5bbbcd283fb38794a1b01b8505cca3dc4
Dmitry Sedykh <sedykh@gmail.com>
2017-05-20 02:53:19 +0300

    replace default JSON encoder

--- encoder.go
+++ encoder.go
@@ -7,27 +7,15 @@ type Encoder func(*Context, interface{}) error
 
 // defaultEncoder is used as the default Encoder.
 func defaultEncoder(c *Context, v interface{}) error {
-	var code = c.Status()
-	var response = &struct {
-		Code     int         `json:"code"`
-		Success  bool        `json:"success"`
-		Error    string      `json:"error,omitempty"`
-		Location string      `json:"location,omitempty"`
-		Data     interface{} `json:"data,omitempty"`
-	}{
-		Code:    code,
-		Success: code < 400,
-	}
-	switch data := v.(type) {
-	case error:
-		response.Error = data.Error()
-	case *RedirectURL:
-		response.Location = data.URL
-	default:
-		response.Data = data
-	}
 	c.SetContentType("application/json; charset=utf-8")
 	enc := json.NewEncoder(c.Response)
 	enc.SetIndent("", "    ")
-	return enc.Encode(response)
+	if err, ok := v.(error); ok {
+		return enc.Encode(&struct {
+			Error string `json:"error,omitempty"`
+		}{
+			Error: err.Error(),
+		})
+	}
+	return enc.Encode(v)
 }
